db207ada6cb896aa8f45cc3c3fbbc46e993889e4
maods <maods@motorola.com>
2019-02-28 16:32:33 +0800

    update meetone rpc node address

--- app/src/main/java/com/maods/bctest/EOS/MEETONEMainNet.java
+++ app/src/main/java/com/maods/bctest/EOS/MEETONEMainNet.java
@@ -6,23 +6,15 @@ package com.maods.bctest.EOS;
 
 public class MEETONEMainNet implements EOSUtils.EosNetParams {
     private static final String[] CANDIDATE_NOTES=new String[]{
-            "http://mainnet.eosio.sg",
-            "http://mars.fn.eosbixin.com",
-            "http://api1.acroeos.one",
-            "https://telosseed.ikuwara.com:8889",
-            "http://bp.cryptolions.io",
-            "http://api.eosn.io",
-            "http://api-mainnet.starteos.io",
-            "http://mainnet.eosnairobi.io",
-            "http://api-meetone.eossv.org",
-            "http://api.nytelos.com",
-            "http://api-eos.blckchnd.com",
-            "http://meetone.eossweden.eu",
-            "http://api.eostribe.io",
-            "http://api-meetone.eosbeijing.one",
-            "http://node1.eosphere.io",
-            "http://mainnet.genereos.io",
-            "http://api.eosvenezuela.io:8888"
+            "https://fullnode.meet.one",
+            "https://api.meetone.eostribe.io",
+            "https://meetseed.ikuwara.com:8889",
+            "https://api.meetone.alohaeos.com",
+            "https://meetone.eossweden.eu",
+            "http://api-meetone.eossf.net:8888",
+            "https://meetone.eosphere.io",
+            "https://meetone.eosn.io",
+            "https://meetone.eosargentina.io"
     };
     @Override
     public String getNetName() {
